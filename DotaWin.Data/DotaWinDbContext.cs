b5517136bf566f443d36078e80361e413b86da2f
BogdanYarotsky <byarotsky@gmail.com>
2022-04-17 22:19:25 +0200

    DbContext scaffolding

--- DotaWin.Data/DotaWinDbContext.cs
+++ DotaWin.Data/DotaWinDbContext.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotaWin.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace DotaWin.Data
 {
     public class DotaWinDbContext : DbContext
     {
+        public DbSet<Update> Updates { get; set; }
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<HeroItem> HeroItems { get; set; }
+
+        // to change
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
 
     }
 }
